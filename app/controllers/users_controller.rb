e556fab75584d282763a1fff3c0d0db97737ceeb
Ashish Sharma <ashish.shm09@gmail.com>
2020-11-06 06:05:45 +0530

    Added flash & user signup integration tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
     render 'new'
     end
-    end
+  end
 
   private
     def user_params
