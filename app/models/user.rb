908bd7e17bf02586a9e13784334be4e721bbbddc
Ashish Sharma <ashish.shm09@gmail.com>
2020-11-09 05:43:34 +0530

    Added tests & non-breaking code to remove subtle bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,11 @@ class User < ApplicationRecord
 
     # Returns true if the given token matches the digest.
     def authenticated?(remember_token)
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+        if remember_digest.nil?
+            false
+        else
+            BCrypt::Password.new(remember_digest).is_password?(remember_token)
+        end
     end
 
     # Forgets a user.
