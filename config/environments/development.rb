3fae77cb799a8a7c96ff948cdcc4911c9d9d89da
Ashish Sharma <ashish.shm09@gmail.com>
2020-11-12 00:29:03 +0530

    Setup mailer & updated authenticated? method & updated signup activation tests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  host = 'localhost:3000'
+  # Local server
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
